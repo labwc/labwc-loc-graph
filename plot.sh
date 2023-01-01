93a61924754174bc26c440cb307bb8a4e3c569a8
Johan Malm <jgm323@gmail.com>
2023-01-01 19:54:24 +0000

    Add tags to graph

--- plot.sh
+++ plot.sh
@@ -14,9 +14,12 @@ set format x "%Y-%m"
 set xlabel "Time"
 set ylabel "LOC (using 'cloc src/')"
 set xtics rotate by -45
+
 set style line 1 lc rgb '#0066aa' pt 0 ps 1 lt 0 lw 0
+set style line 2 lc rgb '#ff0000' pt 7 ps 1 lt 0 lw 0
+
 set xrange ["2020-05-01":"2023-03-31"]
-set yrange ["0":"12000"]
+set yrange ["0":"14000"]
 
 unset key
 
@@ -30,7 +33,9 @@ set style line 12 lc rgb'#808080' lt 0 lw 1
 set grid back ls 12
 EOF
 printf '%b\n' "set output \"$loc_graph\"" >>"$gnuplot_commands"
-printf '%b\n' "plot \"$loc_file\" using 2:5 w p ls 1" >>"$gnuplot_commands"
+printf '%b\n' "plot \"$loc_file\" using 2:5 w p ls 1 ,\\" >>"$gnuplot_commands"
+printf '%b\n' "              \"\" using 2:6 w p ls 2 ,\\" >>"$gnuplot_commands"
+printf '%b\n' "              \"\" using 2:6:7 with labels font \"sans,10\" right offset -0.5,0.5 ls 1" >>"$gnuplot_commands"
 
 trap "rm -f ${gnuplot_commands}" EXIT
 
