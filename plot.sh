7f90e0044d37c5fbf4b407978fd3885120973276
Johan Malm <jgm323@gmail.com>
2023-12-29 22:08:45 +0000

    Extend x/y range

--- plot.sh
+++ plot.sh
@@ -18,8 +18,8 @@ set xtics rotate by -45
 set style line 1 lc rgb '#0066aa' pt 0 ps 1 lt 0 lw 0
 set style line 2 lc rgb '#ff0000' pt 7 ps 1 lt 0 lw 0
 
-set xrange ["2020-05-01":"2024-05-31"]
-set yrange ["0":"19000"]
+set xrange ["2020-05-01":"2025-12-31"]
+set yrange ["0":"25000"]
 
 unset key
 
